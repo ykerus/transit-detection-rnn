8575954bc960a151b7030fd65b97dd8ad05a0992
ykerus <48921025+ykerus@users.noreply.github.com>
2021-06-09 12:36:57 +0200

    Set different peak thresholds monodetection

--- detection/rnn_monodetection.py
+++ detection/rnn_monodetection.py
@@ -4,7 +4,7 @@ from scipy.ndimage import gaussian_filter1d
 from detection import rnn_detection as rnndet
 
 
-def monotransit_detection(pts, conf=None, peak_thresh=0.8, agg_fn=np.multiply, score_fn=np.max, smooth=True):
+def monotransit_detection(pts, conf=None, peak_thresh=0.5, agg_fn=np.multiply, score_fn=np.max, smooth=True):
     # assuming uniform time
     time = np.arange(len(pts)) * utils.min2day(2)
     conf = np.ones_like(pts) if conf is None else conf
@@ -12,7 +12,7 @@ def monotransit_detection(pts, conf=None, peak_thresh=0.8, agg_fn=np.multiply, s
     pts_ = gaussian_filter1d(pts.copy(), 9) if smooth else pts
     conf_ = gaussian_filter1d(conf.copy(), 9) if smooth else pts
     
-    tr_indc = rnndet.get_peaks(pts_>0.8)
+    tr_indc = rnndet.get_peaks(pts_>=peak_thresh)
     
     conf_scores = [score_fn(agg_fn(conf_[indc], pts_[indc])) for indc in tr_indc]
     standard_scores = [score_fn(pts_[indc]) for indc in tr_indc]
@@ -21,8 +21,9 @@ def monotransit_detection(pts, conf=None, peak_thresh=0.8, agg_fn=np.multiply, s
     conf_detections, standard_detections = {}, {}
     for i, indc in enumerate(tr_indc):
         dur_est = time[indc[-1]] - time[indc[0]]
-        conf_detections[conf_scores[i]] = {"t0":tc[i], "duration":dur_est}
         standard_detections[standard_scores[i]] = {"t0":tc[i], "duration":dur_est}
+        if conf_scores[i] >= peak_thresh:
+            conf_detections[conf_scores[i]] = {"t0":tc[i], "duration":dur_est} 
     return standard_detections, conf_detections
     
  
